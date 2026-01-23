2453e93470949b07136adf30a29f8632daa261fc
minahilanwar18-ops <minahilanwar18@gmail.com>
2026-01-24 01:53:29 +0500

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Graphic Designer Portfolio",
+  description: "Creative UI/UX & Graphic Design Portfolio",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
